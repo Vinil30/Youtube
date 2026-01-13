e48d6d06255a7c973bb6bc5c9a7f92d4aec53b71
RSV-exp <2023kucp1041@iiitkota.ac.in>
2026-01-13 20:20:54 +0530

    Removed Predefined Outputs

--- utils/voice_generator.py
+++ utils/voice_generator.py
@@ -9,14 +9,14 @@ class VoiceGenerator:
 
         # AI 1 - Male (VCTK multi-speaker)
         self.tts_ai1 = TTS(
-            model_name="tts_models/en/vctk/vits",
+            model_name="Abhinay45/XTTS-Hindi-finetuned",
             progress_bar=False,
             gpu=False
         )
 
         # AI 2 - Male (VCTK multi-speaker)
         self.tts_ai2 = TTS(
-            model_name="tts_models/en/vctk/vits",
+            model_name="ai4bharat/indic-parler-tts",
             progress_bar=False,
             gpu=False
         )
